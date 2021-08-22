07c4a5809ac0877ad6f56248978d2a39908f0cc8
Yingfeng J <1912449485@qq.com>
2021-08-22 18:49:01 +0800

    '注册功能实现（未完善）'

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -4,11 +4,13 @@
 # @time： 2021/8/20 9:25
 #
 # 进行users 子应用的视图路由
+
 from django.urls import path
-from users.views import RegisterView,ImageCodeView
+from users.views import RegisterView,ImageCodeView,SmsCodeView
 urlpatterns = [
     # path 第一个参数：路由
     # path 第二个参数：视图函数名
     path('register/', RegisterView.as_view(), name = 'register'),
     path('imagecode/',ImageCodeView.as_view(), name = 'imagecode'),
+    path('smscode/',SmsCodeView.as_view(), name = 'smscode'),
 ]
--- blog/users/views.py
+++ blog/users/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponseBadRequest, HttpResponse
+import re
+
+from django.db import DatabaseError
+from django.http import HttpResponseBadRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
+import logging
 
 # Create your views here.
 
@@ -8,13 +12,11 @@ from django.views import View
 from django_redis import get_redis_connection
 
 from libs.captcha.captcha import captcha
+from users.models import User
+from utils.response_code import RETCODE
 
+logger=logging.getLogger('django')
 
-class RegisterView(View):
-
-    def get(self, request):
-
-        return render(request, 'register.html')
 
 
 class ImageCodeView(View):
@@ -45,3 +47,101 @@ class ImageCodeView(View):
         # 返回响应，将生成的图片以content_type为image/jpeg的形式返回给请求
         return HttpResponse(image, content_type='image/jpeg')
 
+class RegisterView(View):
+
+    def get(self, request):
+
+        return render(request, 'register.html')
+
+    def post(self, request):
+        #
+        # 1.接收数据
+        mobile = request.POST.get('mobile')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        # image_code_client = request.GET.get('image_code')
+        # uuid = request.GET.get('uuid')
+
+
+
+        # 2.验证数据
+        #     2.1参数是否齐全
+        # 校验参数
+        if not all([mobile,password,password2]):
+            return JsonResponse({'code': RETCODE.NECESSARYPARAMERR, 'errmsg': '缺少必传参数'})
+
+
+        #     2.2手机号的格式是否正确
+        if not re.match(r'^1[3-9]\d{9}$',mobile):
+            return HttpResponseBadRequest('请输入正确的手机号')
+
+        #     2.3密码是否符合格式
+        if not re.match(r'^[0-9a-zA-Z]{8,20}$',password):
+            return HttpResponseBadRequest('请输入8-20位密码')
+
+        #     2.4密码和确认密码要一致
+        if password != password2:
+            return HttpResponseBadRequest('两次密码输入不一致')
+
+        # #     2.5图片验证码是否和redis中的一致
+        # # 创建连接到redis的对象
+        # redis_conn = get_redis_connection('default')
+        # # 提取图形验证码
+        # image_code_server = redis_conn.get('img:%s' % uuid)
+        # if image_code_server is None:
+        #     # 图形验证码过期或者不存在
+        #     return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码失效'})
+        # # 删除图形验证码，避免恶意测试图形验证码
+        # try:
+        #     redis_conn.delete('img:%s' % uuid)
+        # except Exception as e:
+        #     logger.error(e)
+        # # 对比图形验证码
+        # image_code_server = image_code_server.decode()  # bytes转字符串
+        # if image_code_client.lower() != image_code_server.lower():  # 转小写后比较
+        #     return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '输入图形验证码有误'})
+
+        # 3.保存法册信息
+        # create_user 使用系统的方法对密码进行加密
+        try:
+            user = User.objects.create_user(username=mobile,
+                                            mobile = mobile,
+                                            password=password)
+        except  DatabaseError as e:
+            logger.error(e)
+            return HttpResponseBadRequest('注册失败')
+
+        # 4.返回响应跳转到指定页面
+        return HttpResponse('注册成功，跳转至首页（未实现）')
+
+class SmsCodeView(View):
+
+    def get(self,request):
+        # 接收参数
+        image_code_client = request.GET.get('image_code')
+        uuid = request.GET.get('uuid')
+        mobile=request.GET.get('mobile')
+
+        # 校验参数
+        if not all([image_code_client, uuid,mobile]):
+            return JsonResponse({'code': RETCODE.NECESSARYPARAMERR, 'errmsg': '缺少必传参数'})
+
+        # 创建连接到redis的对象
+        redis_conn = get_redis_connection('default')
+        # 提取图形验证码
+        image_code_server = redis_conn.get('img:%s' % uuid)
+        if image_code_server is None:
+            # 图形验证码过期或者不存在
+            return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码失效'})
+        # 删除图形验证码，避免恶意测试图形验证码
+        try:
+            redis_conn.delete('img:%s' % uuid)
+        except Exception as e:
+            logger.error(e)
+        # 对比图形验证码
+        image_code_server = image_code_server.decode()  # bytes转字符串
+        if image_code_client.lower() != image_code_server.lower():  # 转小写后比较
+            return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '输入图形验证码有误'})
+
+        return JsonResponse({'code': RETCODE.OK, 'errmsg': '发送短信成功'})
